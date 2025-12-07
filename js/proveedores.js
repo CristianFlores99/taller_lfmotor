5c1603b95102791dab1db5b533ef3f54bf8be753
Cristian Fernando Flores <cristianfernandoflores@outlook.com>
2025-12-07 11:21:34 -0300

    actualizar sistema 2

--- js/proveedores.js
+++ js/proveedores.js
@@ -1,5 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 const supabaseUrl = 'https://ovfsffckhzelgbgohakv.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92ZnNmZmNraHplbGdiZ29oYWt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2NTA0MjYsImV4cCI6MjA3NjIyNjQyNn0.hDiIhAHAr04Uo9todWdk0QUaqD3RYj5kMkITavzPiHc';
 const supabase = createClient(supabaseUrl, supabaseKey);
